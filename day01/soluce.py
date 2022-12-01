5d3a54d9a9928cfd52feee2e688410aeffc0aa7b
Yanzi-dev <106020312+Yanzi-dev@users.noreply.github.com>
2022-12-01 18:19:44 +0100

    Day 1 python

---
+++ day01/soluce.py
@@ -0,0 +1,26 @@
+def main():
+  # init
+  hand = open('input.txt')
+    i = 0
+    calories = dict()
+    for line in hand:
+        if line != '\n':
+            calories.setdefault(i, []).append(line.strip())
+        else:
+            i += 1
+
+    # transformation
+    for i, a in calories.items():
+        calories[i] = sum(map(int, a))
+
+    # max calories
+    print(max(calories.values()))
+
+    # sum calories of 3 top calories
+    calories_values = list(calories.values())
+    calories_values.sort(reverse=True)
+    print(sum(calories_values[0:3]))
+
+
+main()
+quit()
